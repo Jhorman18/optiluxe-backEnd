91d1549a71d44f0ba56b9bd864c51306026bca3f
Jhorman Cortes <jhormanc150@gmail.com>
2026-04-27 10:24:54 -0500

    configuracion despliegue

--- src/middlewares/cors.middleware.js
+++ src/middlewares/cors.middleware.js
@@ -4,7 +4,8 @@ const corsOptions = {
     origin: [
         "http://localhost:5173",
         "http://localhost:3000",
-    ],
+        process.env.FRONTEND_URL,
+    ].filter(Boolean),
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
